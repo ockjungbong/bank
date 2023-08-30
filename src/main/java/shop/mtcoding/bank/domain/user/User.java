fd3ffa0e7d8371b161b70519d749f3a8ab6c3b7a
devock <ockjungbong@gmail.com>
2023-08-30 23:10:48 +0900

    mariadb로 변경 및 테이블 생성 확인

--- src/main/java/shop/mtcoding/bank/domain/user/User.java
+++ src/main/java/shop/mtcoding/bank/domain/user/User.java
@@ -5,6 +5,7 @@ import java.time.LocalDateTime;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EntityListeners;
+import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
