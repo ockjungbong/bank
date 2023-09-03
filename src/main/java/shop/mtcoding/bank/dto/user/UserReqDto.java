2b56d62aced8ef32a5655aabd10512efeb5dbb97
devock <ockjungbong@gmail.com>
2023-09-03 16:29:50 +0900

    회원가입 컨트롤러 정규표현식 DTO 적용

--- src/main/java/shop/mtcoding/bank/dto/user/UserReqDto.java
+++ src/main/java/shop/mtcoding/bank/dto/user/UserReqDto.java
@@ -1,6 +1,8 @@
 package shop.mtcoding.bank.dto.user;
 
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
@@ -13,16 +15,20 @@ public class UserReqDto {
   @Getter
   @Setter
   public static class JoinReqDto {
+    @Pattern(regexp = "^[a-zA-Z0-9]{2,20}$", message = "영문/숫자 2~20자 이내로 작성해 주세요.")
     @NotEmpty // null이거나 공백일 수 없다.
     private String username;
 
     @NotEmpty
+    @Size(min = 4, max = 20)
     private String password;
 
+    @Pattern(regexp = "^[0-9a-zA-Z]([-_\\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\\.]?[0-9a-zA-Z])*\\.[a-zA-Z]{2,3}$", message = "이메일 형식으로 작성해 주세요.")
     @NotEmpty
     private String email;
 
     @NotEmpty
+    @Pattern(regexp = "^[a-zA-Z\uAC00-\uD7A3]{1,20}$", message = "한글/영문 1~20자 이내로 작성해 주세요.")
     private String fullname;
 
     // 나열된 필드값으로 엔티티 생성
