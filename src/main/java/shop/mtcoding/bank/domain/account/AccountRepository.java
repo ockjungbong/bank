d79d40a04742a8766c04cf4aa97c5d4c6dcc3bd9
devock <ockjungbong@gmail.com>
2023-09-11 22:59:53 +0900

    계좌등록 서비스 만들기

--- src/main/java/shop/mtcoding/bank/domain/account/AccountRepository.java
+++ src/main/java/shop/mtcoding/bank/domain/account/AccountRepository.java
@@ -1,7 +1,13 @@
 package shop.mtcoding.bank.domain.account;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AccountRepository extends JpaRepository<Account, Long>{
+public interface AccountRepository extends JpaRepository<Account, Long> {
+
+  // jpa query method
+  // checkpoint : 리팩토링 해야 함
+  Optional<Account> findByNumber(Long number);
 
 }
---
+++ src/main/java/shop/mtcoding/bank/service/AccountService.java
@@ -0,0 +1,84 @@
+package shop.mtcoding.bank.service;
+
+import java.util.Optional;
+
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.NotNull;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import shop.mtcoding.bank.domain.account.Account;
+import shop.mtcoding.bank.domain.account.AccountRepository;
+import shop.mtcoding.bank.domain.user.User;
+import shop.mtcoding.bank.domain.user.UserRepository;
+import shop.mtcoding.bank.handler.ex.CustomApiException;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class AccountService {
+
+  private final UserRepository userRepository;
+  private final AccountRepository accountRepository;
+
+  @Transactional
+  public AccountSaveRsspDto 계좌등록(AccountSaveReqDto accountSaveReqDto, Long userId) {
+    // User가 DB에 있는지 검증
+    User userPS = userRepository.findById(userId).orElseThrow(
+        () -> new CustomApiException("유저를 찾을 수 없습니다."));
+
+    // 해당 계좌가 DB에 있는 중복여부를 체크
+    Optional<Account> accountOP = accountRepository.findByNumber(accountSaveReqDto.getNumber());
+    if (accountOP.isPresent()) {
+      throw new CustomApiException("해당 계좌가 이미 존재합니다.");
+    }
+
+    // 계좌 등록
+    Account accountPS = accountRepository.save(accountSaveReqDto.toEntity(userPS));
+
+    // DTO를 응답
+    return new AccountSaveRsspDto(accountPS);
+
+  }
+
+  @Getter
+  @Setter
+  public static class AccountSaveRsspDto {
+    private Long id;
+    private Long number;
+    private Long balance;
+
+    public AccountSaveRsspDto(Account account) {
+      this.id = account.getId();
+      this.number = account.getNumber();
+      this.balance = account.getBalance();
+    }
+
+  }
+
+  @Getter
+  @Setter
+  public static class AccountSaveReqDto {
+    @NotNull
+    @Digits(integer = 4, fraction = 4)
+    private Long number;
+
+    @NotNull
+    @Digits(integer = 4, fraction = 4)
+    private Long password;
+
+    public Account toEntity(User user) {
+      return Account.builder()
+          .number(number)
+          .password(password)
+          .balance(1000L)
+          .user(user)
+          .build();
+    }
+  }
+
+}
