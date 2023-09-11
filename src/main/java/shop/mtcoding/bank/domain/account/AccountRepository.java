41074425bf632790c9cd37d6c2949095bb21162d
devock <ockjungbong@gmail.com>
2023-09-12 00:28:30 +0900

    계좌등록 컨트롤러 테스트 완료

--- src/main/java/shop/mtcoding/bank/domain/account/AccountRepository.java
+++ src/main/java/shop/mtcoding/bank/domain/account/AccountRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface AccountRepository extends JpaRepository<Account, Long> {
 
   // jpa query method
-  // checkpoint : 리팩토링 해야 함
+  // checkpoint : 리팩토링 해야 함(계좌 소유자 확인시에 쿼리가 두 번 나가지 때문에 join fetch)
   Optional<Account> findByNumber(Long number);
 
 }
