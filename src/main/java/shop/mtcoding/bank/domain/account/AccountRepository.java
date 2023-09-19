cb5be97b1826e720993ca6d6e82de57c233108ce
devock <ockjungbong@gmail.com>
2023-09-19 23:27:16 +0900

    계좌목록보기 서비스 만들기

--- src/main/java/shop/mtcoding/bank/domain/account/AccountRepository.java
+++ src/main/java/shop/mtcoding/bank/domain/account/AccountRepository.java
@@ -1,5 +1,6 @@
 package shop.mtcoding.bank.domain.account;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,4 +11,7 @@ public interface AccountRepository extends JpaRepository<Account, Long> {
   // checkpoint : 리팩토링 해야 함(계좌 소유자 확인시에 쿼리가 두 번 나가지 때문에 join fetch)
   Optional<Account> findByNumber(Long number);
 
+  // jpa query method
+  // select * from account where user_id = :id;
+  List<Account> findByUser_id(Long id);
 }
--- src/main/java/shop/mtcoding/bank/service/AccountService.java
+++ src/main/java/shop/mtcoding/bank/service/AccountService.java
@@ -1,11 +1,16 @@
 package shop.mtcoding.bank.service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 import shop.mtcoding.bank.domain.account.Account;
 import shop.mtcoding.bank.domain.account.AccountRepository;
 import shop.mtcoding.bank.domain.user.User;
@@ -13,33 +18,71 @@ import shop.mtcoding.bank.domain.user.UserRepository;
 import shop.mtcoding.bank.dto.account.AccountReqDto.AccountSaveReqDto;
 import shop.mtcoding.bank.dto.account.AccountRespDto.AccountSaveRespDto;
 import shop.mtcoding.bank.handler.ex.CustomApiException;
+import shop.mtcoding.bank.service.AccountService.AccountListRespDto.AccountDto;
 
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class AccountService {
 
-  private final UserRepository userRepository;
-  private final AccountRepository accountRepository;
+    private final UserRepository userRepository;
+    private final AccountRepository accountRepository;
 
-  @Transactional
-  public AccountSaveRespDto 계좌등록(AccountSaveReqDto accountSaveReqDto, Long userId) {
-    // User가 DB에 있는지 검증
-    User userPS = userRepository.findById(userId).orElseThrow(
-        () -> new CustomApiException("유저를 찾을 수 없습니다."));
+    public AccountListRespDto 계좌목록보기_유저별(Long userId) {
+        User userPS = userRepository.findById(userId).orElseThrow(
+                () -> new CustomApiException("유저를 찾을 수 없습니다."));
 
-    // 해당 계좌가 DB에 있는 중복여부를 체크
-    Optional<Account> accountOP = accountRepository.findByNumber(accountSaveReqDto.getNumber());
-    if (accountOP.isPresent()) {
-      throw new CustomApiException("해당 계좌가 이미 존재합니다.");
+        // 유저의 모든 계좌목록
+        List<Account> accountListPS = accountRepository.findByUser_id(userId);
+        return new AccountListRespDto(userPS, accountListPS);
+
     }
 
-    // 계좌 등록
-    Account accountPS = accountRepository.save(accountSaveReqDto.toEntity(userPS));
+    @Getter
+    @Setter
+    public static class AccountListRespDto {
 
-    // DTO를 응답
-    return new AccountSaveRespDto(accountPS);
+        private String fullname;
+        private List<AccountDto> accounts = new ArrayList<>();
 
-  }
+        public AccountListRespDto(User user, List<Account> accounts) {
+            this.fullname = user.getFullname();
+            this.accounts = accounts.stream().map(AccountDto::new).collect(Collectors.toList());
+        }
+
+        @Setter
+        @Getter
+        public class AccountDto {
+            private Long id;
+            private Long number;
+            private Long balance;
+
+            public AccountDto(Account account) {
+                this.id = account.getId();
+                this.number = account.getNumber();
+                this.balance = account.getBalance();
+            }
+        }
+    }
+
+    @Transactional
+    public AccountSaveRespDto 계좌등록(AccountSaveReqDto accountSaveReqDto, Long userId) {
+        // User가 DB에 있는지 검증
+        User userPS = userRepository.findById(userId).orElseThrow(
+                () -> new CustomApiException("유저를 찾을 수 없습니다."));
+
+        // 해당 계좌가 DB에 있는 중복여부를 체크
+        Optional<Account> accountOP = accountRepository.findByNumber(accountSaveReqDto.getNumber());
+        if (accountOP.isPresent()) {
+            throw new CustomApiException("해당 계좌가 이미 존재합니다.");
+        }
+
+        // 계좌 등록
+        Account accountPS = accountRepository.save(accountSaveReqDto.toEntity(userPS));
+
+        // DTO를 응답
+        return new AccountSaveRespDto(accountPS);
+
+    }
 
 }
